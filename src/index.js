9a12fda126c3fc9e11582ccc67c74b4a7ab7339e
webcoding.by <webcoding.by@gmail.com>
2020-07-22 23:57:11 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export default function App() {
+  return <div>Hello React Warrior</div>;
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
